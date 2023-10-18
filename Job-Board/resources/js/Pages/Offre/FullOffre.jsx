a336f27b681b4639cbe45a5fb444035a98b43b90
matthieujulie07 <matthieujulie07@gmail.com>
2023-10-18 17:39:28 +0200

    add form for guest

--- Job-Board/resources/js/Pages/Offre/FullOffre.jsx
+++ Job-Board/resources/js/Pages/Offre/FullOffre.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import CheckLayout from "../../Layouts/CheckLayout";
+import PrimaryButton from "../../../../vendor/laravel/breeze/stubs/inertia-react/resources/js/Components/PrimaryButton";
+import FormJob from "@/Components/FormJob";
 
 const FullOffre = (props) => {
+    let [formVisibility, SetFormVisibility] = useState(true);
+
+    let form;
+    if (formVisibility) {
+        form = <FormJob></FormJob>;
+    } else {
+        form = null;
+    }
+
     return (
         <CheckLayout auth={props.auth} title={props.title}>
             <div>
                 <div>Companie ID : {props.company_id}</div>
                 <div>Description complete : {props.full_description} </div>
             </div>
+            <PrimaryButton
+                className=" float-right"
+                onClick={() => SetFormVisibility(!formVisibility)}
+            >
+                Postuler
+            </PrimaryButton>
+            {form}
         </CheckLayout>
     );
 };
