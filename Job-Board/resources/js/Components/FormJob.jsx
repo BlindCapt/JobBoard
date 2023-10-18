3418e6e09e958ba3f1c4cf3c86210d97c113f7fa
matthieujulie07 <matthieujulie07@gmail.com>
2023-10-18 17:46:01 +0200

    change color form button

--- Job-Board/resources/js/Components/FormJob.jsx
+++ Job-Board/resources/js/Components/FormJob.jsx
@@ -26,7 +26,7 @@ const FormJob = () => {
                                     name="first-name"
                                     id="first-name"
                                     autoComplete="given-name"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-400 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -44,7 +44,7 @@ const FormJob = () => {
                                     name="last-name"
                                     id="last-name"
                                     autoComplete="family-name"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-400 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -62,7 +62,7 @@ const FormJob = () => {
                                     name="email"
                                     type="email"
                                     autoComplete="email"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-400 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -80,7 +80,7 @@ const FormJob = () => {
                                     name="street-address"
                                     id="street-address"
                                     autoComplete="street-address"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-400 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -98,7 +98,7 @@ const FormJob = () => {
                                     name="city"
                                     id="city"
                                     autoComplete="address-level2"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-400 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -116,7 +116,7 @@ const FormJob = () => {
                                     name="postal-code"
                                     id="postal-code"
                                     autoComplete="postal-code"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-400 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -134,7 +134,7 @@ const FormJob = () => {
                                             id="about"
                                             name="about"
                                             rows={3}
-                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-400 sm:text-sm sm:leading-6"
                                             defaultValue={""}
                                         />
                                     </div>
@@ -164,7 +164,7 @@ const FormJob = () => {
                                             id="candidates"
                                             name="candidates"
                                             type="checkbox"
-                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                            className="h-4 w-4 rounded border-gray-300 text-green-400 focus:ring-green-400"
                                         />
                                     </div>
                                     <div className="text-sm leading-6">
@@ -186,7 +186,7 @@ const FormJob = () => {
                                             id="offers"
                                             name="offers"
                                             type="checkbox"
-                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                            className="h-4 w-4 rounded border-gray-300 text-green-400 focus:ring-green-400"
                                         />
                                     </div>
                                     <div className="text-sm leading-6">
@@ -218,7 +218,7 @@ const FormJob = () => {
                                         id="push-everything"
                                         name="push-notifications"
                                         type="radio"
-                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                        className="h-4 w-4 border-gray-300 text-green-400 focus:ring-green-400"
                                     />
                                     <label
                                         htmlFor="push-everything"
@@ -232,7 +232,7 @@ const FormJob = () => {
                                         id="push-email"
                                         name="push-notifications"
                                         type="radio"
-                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                        className="h-4 w-4 border-gray-300 text-green-400 focus:ring-green-400"
                                     />
                                     <label
                                         htmlFor="push-email"
@@ -246,7 +246,7 @@ const FormJob = () => {
                                         id="push-nothing"
                                         name="push-notifications"
                                         type="radio"
-                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                        className="h-4 w-4 border-gray-300 text-green-400 focus:ring-green-400"
                                     />
                                     <label
                                         htmlFor="push-nothing"
@@ -263,14 +263,8 @@ const FormJob = () => {
 
             <div className="mt-6 flex items-center justify-end gap-x-6">
                 <button
-                    type="button"
-                    className="text-sm font-semibold leading-6"
-                >
-                    Cancel
-                </button>
-                <button
                     type="submit"
-                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-lg bg-green-400 px-12 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-300 "
                 >
                     Save
                 </button>
