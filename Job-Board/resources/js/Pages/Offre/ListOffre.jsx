f05a875f4735e8850dccef6db69d2c33e0a92bae
matthieujulie07 <matthieujulie07@gmail.com>
2023-10-19 14:55:03 +0200

    fix style listOffre

--- Job-Board/resources/js/Pages/Offre/ListOffre.jsx
+++ Job-Board/resources/js/Pages/Offre/ListOffre.jsx
@@ -5,25 +5,27 @@ import SearchBar from "@/Components/SearchBar";
 
 export default function ListOffre({ auth, data }) {
     const [NbAd, SetNbAd] = useState(5);
-    const [search, setSearch] = useState('');
-    const index = [5, 10, 15]; 
+    const [search, setSearch] = useState("");
+    const index = [5, 10, 15];
     const filteredData = data.filter((item) =>
         item[0].toLowerCase().includes(search.toLowerCase())
     );
-    const Arr = filteredData.slice(0, NbAd).map((item, index) => (
-        <Advertisement
-            titre={item[0]}
-            description={item[1]}
-            id={item[2]}
-            key={index}
-        />
-    ));
+    const Arr = filteredData
+        .slice(0, NbAd)
+        .map((item, index) => (
+            <Advertisement
+                titre={item[0]}
+                description={item[1]}
+                id={item[2]}
+                key={index}
+            />
+        ));
 
     return (
         <CheckLayout auth={auth} title={"offres d'emploi"}>
-            <div className="container mx-auto">
-                <div className="flex gap-5 mb-5">
-                    <SearchBar search={search} setSearch={setSearch} /> 
+            <div className="container mx-auto p-8">
+                <div className="flex gap-5 mb-5 pl-8 bg-asideGrey p-2 rounded-lg">
+                    <SearchBar search={search} setSearch={setSearch} />
                     <select
                         className="rounded-xl bg-background my-2"
                         onChange={(e) => SetNbAd(e.target.value)}
@@ -39,9 +41,8 @@ export default function ListOffre({ auth, data }) {
                         ))}
                     </select>
                 </div>
-                <div className="p-4 bg-asideGrey rounded-md">{Arr}</div>
+                <div className="p-4 pb-12 bg-asideGrey rounded-lg">{Arr}</div>
             </div>
         </CheckLayout>
     );
 }
-
